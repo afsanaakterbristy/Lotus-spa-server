8fe01e4b84bc8b7486147df221670be3830a7267
afsanaakterbristy <afsanaakter.bristy28@gmail.com>
2022-11-08 12:41:24 +0600

    added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,7 +17,63 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
+async function run() {
+    try {
+         const serviceCollection = client.db('serviceSpa').collection('services');
 
+         const reviewCollection = client.db('serviceSpa').collection('review');
+
+        
+      
+        
+        //data get for 3 services
+        app.get('/services', async (req, res) => {
+        const query = {}
+        const cursor = serviceCollection.find(query);
+        const services = await cursor.limit(3).toArray();
+        res.send(services);
+        });
+
+        //data get allservices
+        app.get('/allservices', async (req, res) => {
+        const query = {}
+        const cursor = serviceCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+        });
+
+        //data get for specifyc services
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query={_id : ObjectId(id)}
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+        })
+        
+        // app.get('/allservices/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query={_id : ObjectId(id)}
+        //     const service = await serviceCollection.findOne(query);
+        //     res.send(service)
+        //  })
+
+      //review  api for post 
+        app.post('/review', async (req, rea) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+
+       })
+
+    }
+    
+    finally {
+        
+    }
+}
+
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('server is running')
