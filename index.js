f1aeccdb39a2548a4d0dc5bf9e585b079b2679a5
afsanaakterbristy <afsanaakter.bristy28@gmail.com>
2022-11-08 10:16:59 +0600

    added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,14 @@ const port = process.env.PORT || 5000;
 //middle wares
 app.use(cors());
 app.use(express.json());
+
+
+
+//database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@bristy.ogzpuzu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 
 
